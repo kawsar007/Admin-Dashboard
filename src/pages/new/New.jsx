71a0514a6d6c99d2cf76fd279cc8d34a1462b522
kawsar007 <imkawsar007@gmail.com>
2022-05-12 18:55:05 +0600

    initial commit

---
+++ src/pages/new/New.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./new.scss"
+
+const New = () => {
+  return (
+    <div className='new'>
+        <h1 className='title'>New Page</h1>
+    </div>
+  )
+}
+
+export default New;
